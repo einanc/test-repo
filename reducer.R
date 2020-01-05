ef4ccd16e338145f0c9c0d19c61b3fcde42a6c8d
Emir Inanc <inance@mef.edu.tr>
2020-01-06 02:03:21 +0300

    Update reducer.R

--- reducer.R
+++ reducer.R
@@ -1,53 +1,23 @@
-
-con <- file("https://raw.githubusercontent.com/einanc/test-repo/master/ontime_flights.tsv", open = "r")
-
-while (length(lines <- readLines(con, n = 1, warn = FALSE)) > 0) {
-  fields <- fields <- unlist(strsplit(lines, split = "\\t", perl = TRUE))
-  year <- substr(fields[1],start = 1,stop = 4)
-  month <- substr(fields[1],start = 6,stop = 7)
-  airlineID <- as.numeric(fields[2])
-  deptDelay <- as.numeric(fields[7])
-  cat(paste(airlineID, "|", date, "|", month, sep=""), "\t", deptDelay, "\n")
-}
-
-close(con)
-
-con <- file("stdin")
-#com <- file("mapped")
-liness <- readLines(con)
-close(con)
-
-keyval <- list()
-
-for (i in seq_along(liness))
-{
-  linex <- unlist(strsplit(liness[i], split = "\t"))
-  key <- linex[1]
-  val <- as.numeric(linex[2])
-  
-  cur.maxval <- keyval[[key]]
-  
-  if (is.null(cur.maxval))
-  {   
-    keyval[[key]] <- val 
-  }
-  else
-  {   
-    if (val > cur.maxval)
-    {
-      keyval[[key]] <- val 
+con <- file("stdin", open = "r")
+delays <- numeric(0) # vector of departure delays
+lastKey <- ""
+while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0) {
+split <- unlist(strsplit("19690|2014|04 	 -50", "\t"))
+key <- split[[1]]
+deptDelay <- as.numeric(split[[2]])
+    
+    # Start of a new key, so output results for previous key:
+    if (!(identical(lastKey, "")) & (!(identical(lastKey, key)))) {
+      keySplit <- unlist(strsplit(lastKey, "\\|"))
+      cat(keySplit[[2]], "\t", keySplit[[3]], "\t", length(delays), "\t", keySplit[[1]], "\t", (mean(delays)), "\n")
+      lastKey <- key
+      delays <- c(deptDelay) 
+    } else { # Still working on same key so append dept delay value to vector:
+        lastKey <- key
+      delays <- c(delays, deptDelay)
     }
   }
-}
-
-keys <- as.numeric(names(keyval))
-vals <- as.numeric(unlist(keyval))
-
-output <- matrix(c(keys, vals), ncol = 2)
-output <- output[order(keys),, drop = F]
-
-for (i in seq_along(output[,1]))
-{
-  pasted <- paste(output[i,], collapse = "\t")
-  cat(sprintf("%s\n", pasted))
-}
+  
+  # We're done, output last record:
+keySplit <- unlist(strsplit(lastKey, "\\|"))
+cat(keySplit[[2]], "\t", keySplit[[3]], "\t", length(delays), "\t", keySplit[[1]], "\t", (mean(delays)), "\n")
